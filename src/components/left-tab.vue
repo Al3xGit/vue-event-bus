f63eb96647896325ab9eb294584c2f299ee83ce2
Alex <alexandre.meur@protonmail.com>
2020-02-22 23:49:19 +0100

    Added event bus and several events

--- src/components/left-tab.vue
+++ src/components/left-tab.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
     <h2>Left Tab</h2>
+    <p>{{paragraphMessage}}</p>
+    <button v-on:click="emitDoubleEvent()">Double counter in right tab</button>
   </div>
 </template>
 
 <script>
+import { EventBus } from '../service/EventBus.js';
+
 export default {
   name: 'LeftTab',
+  data: function() {
+    return {
+      displayClick: 0,
+      paragraphMessage: '...',
+    }
+  },
+  methods: {
+    emitDoubleEvent() {
+      EventBus.$emit('DOUBLE_COUNTER');
+    },
+  },
+  created: function() {
+    EventBus.$on('COUNTER', nbClick => {
+      this.paragraphMessage = `The counter in right panel is at ${nbClick}` ;
+    });
+  }
 }
 </script>
 
--- src/components/right-tab.vue
+++ src/components/right-tab.vue
@@ -1,22 +1,54 @@
 <template>
   <div>
     <h2>Right Tab</h2>
-    <p>Counter is at {{counter}}.</p>
-    <button type="button" name="button" v-on:click="counter++">Increase counter</button>
+    <p class="counter-text">Counter : <span class="counter-amount">{{counter}}</span></p>
+    <button v-on:click="increaseCounter()">Increase counter</button>
+    <button v-on:click="resetCounter()">Reset counter</button>
   </div>
 </template>
 
 <script>
+import { EventBus } from '../service/EventBus.js';
+
 export default {
   name: 'RightTab',
   data: function() {
     return {
-      counter: 1,
+      counter: 0,
     };
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+      this.emitCounter();
+    },
+    resetCounter() {
+      this.counter = 0;
+      this.emitCounter();
+    },
+    emitCounter() {
+      EventBus.$emit('COUNTER', this.counter);
+      console.log(`emiting COUNTER ${this.counter}`);
+    }
+  },
+  created: function() {
+    EventBus.$on('DOUBLE_COUNTER', () => {
+      this.counter*=2;
+      this.emitCounter();
+    });
+    this.emitCounter();
   },
 }
 </script>
 
 <style scoped>
-
+  .counter-text {
+    font-weight: bold;
+  }
+  .counter-amount {
+    padding: 3px 5px;
+    border: 2px solid lightblue;
+    border-radius: 3px;
+    background-color: lightgrey;
+  }
 </style>

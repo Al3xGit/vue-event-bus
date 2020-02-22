66f7142c0160997f464e26dcede406c80262bd8f
Alex <alexandre.meur@protonmail.com>
2020-02-22 22:27:10 +0100

    Added left tab

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Test</h1>
+    <left-tab></left-tab>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LeftTab from './components/left-tab.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'left-tab': LeftTab,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/left-tab.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>I am left tab</h2>
+</template>
+
+<script>
+export default {
+  name: 'LeftTab',
+}
+</script>
+
+<style>
+  h2 {
+    color: grey;
+  }
+
+</style>

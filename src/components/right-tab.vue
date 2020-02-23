12530820b6679ad40a418106c6c652b03acd029e
Alex <alexandre.meur@protonmail.com>
2020-02-23 22:09:09 +0100

    Indentation right tab

--- src/components/right-tab.vue
+++ src/components/right-tab.vue
@@ -1,54 +1,63 @@
 <template>
   <div>
     <h2>Right Tab</h2>
-    <p class="counter-text">Counter : <span class="counter-amount">{{counter}}</span></p>
+
+    <p class="counter-text">
+      Counter :
+      <span class="counter-amount">{{counter}}</span>
+    </p>
+
     <button v-on:click="increaseCounter()">Increase counter</button>
     <button v-on:click="resetCounter()">Reset counter</button>
   </div>
 </template>
 
 <script>
-import { EventBus } from '../service/EventBus.js';
+import { EventBus } from "../service/EventBus.js";
 
 export default {
-  name: 'RightTab',
+  name: "RightTab",
+
   data: function() {
     return {
-      counter: 0,
+      counter: 0
     };
   },
+
   methods: {
     increaseCounter() {
       this.counter++;
       this.emitCounter();
     },
+
     resetCounter() {
       this.counter = 0;
       this.emitCounter();
     },
+
     emitCounter() {
-      EventBus.$emit('COUNTER', this.counter);
+      EventBus.$emit("COUNTER", this.counter);
       console.log(`emiting COUNTER ${this.counter}`);
     }
   },
   created: function() {
-    EventBus.$on('DOUBLE_COUNTER', () => {
-      this.counter*=2;
+    EventBus.$on("DOUBLE_COUNTER", () => {
+      this.counter *= 2;
       this.emitCounter();
     });
     this.emitCounter();
-  },
-}
+  }
+};
 </script>
 
 <style scoped>
-  .counter-text {
-    font-weight: bold;
-  }
-  .counter-amount {
-    padding: 3px 5px;
-    border: 2px solid lightblue;
-    border-radius: 3px;
-    background-color: lightgrey;
-  }
+.counter-text {
+  font-weight: bold;
+}
+.counter-amount {
+  padding: 3px 5px;
+  border: 2px solid lightblue;
+  border-radius: 3px;
+  background-color: lightgrey;
+}
 </style>
